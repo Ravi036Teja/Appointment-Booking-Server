d779b4602d13cf0cbb2b6987dcde1917f4f8021b
Ravi036 <rteja6067@gmail.com>
2025-07-30 13:38:07 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -14,5 +14,10 @@ app.use("/api/bookings", require("./routes/bookingRoute"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/blocked", require("./routes/blockedSlotRoute"))
 
+// ✅ Root route
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
